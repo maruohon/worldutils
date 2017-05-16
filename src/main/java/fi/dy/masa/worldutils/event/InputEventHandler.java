4463e7c74c866a0a9187c5c46984bd5600667376
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-05-16 20:25:57 +0300

    Fix modifier keys getting stuck if opening a GUI while sneaking

--- src/main/java/fi/dy/masa/worldutils/event/InputEventHandler.java
+++ src/main/java/fi/dy/masa/worldutils/event/InputEventHandler.java
@@ -2,6 +2,7 @@ package fi.dy.masa.worldutils.event;
 
 import org.lwjgl.input.Keyboard;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -118,6 +119,17 @@ public class InputEventHandler
                 }
             }
         }
+    }
+
+    @SubscribeEvent
+    public void onGuiOpenEvent(GuiOpenEvent event)
+    {
+        // Reset the scrolling modifier when the player opens a GUI.
+        // Otherwise the key up event will get eaten and our scrolling mode will get stuck on
+        // until the player sneaks again.
+        // FIXME Apparently there are key input events for GUI screens in 1.8,
+        // so this probably can be removed then.
+        resetModifiers();
     }
 
     static
