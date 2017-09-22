54b9bb2ef18357c0eea28ffd157cc6afc70bf689
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-09-22 14:16:52 +0300

    Bump version to 1.12.2-0.4.0. Sign the JAR.

--- src/main/java/fi/dy/masa/worldutils/WorldUtils.java
+++ src/main/java/fi/dy/masa/worldutils/WorldUtils.java
@@ -1,11 +1,13 @@
 package fi.dy.masa.worldutils;
 
 import java.io.File;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import fi.dy.masa.worldutils.command.CommandWorldUtils;
@@ -14,7 +16,7 @@ import fi.dy.masa.worldutils.network.PacketHandler;
 import fi.dy.masa.worldutils.proxy.IProxy;
 import fi.dy.masa.worldutils.reference.Reference;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT,
     guiFactory = "fi.dy.masa.worldutils.config.WorldUtilsGuiFactory",
     updateJSON = "https://raw.githubusercontent.com/maruohon/worldutils/master/update.json",
     acceptableRemoteVersions = "*",
@@ -27,13 +29,13 @@ public class WorldUtils
 
     @SidedProxy(clientSide = Reference.PROXY_CLASS_CLIENT, serverSide = Reference.PROXY_CLASS_SERVER)
     public static IProxy proxy;
-    public static Logger logger;
+
+    public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
     public static String configDirPath;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
         Configs.loadConfigsFromFile(event.getSuggestedConfigurationFile());
         configDirPath = new File(event.getModConfigurationDirectory(), Reference.MOD_ID).getAbsolutePath();
 
@@ -47,5 +49,24 @@ public class WorldUtils
     public void serverStarting(FMLServerStartingEvent event)
     {
         event.registerServerCommand(new CommandWorldUtils());
+    }
+
+    @Mod.EventHandler
+    public void onFingerPrintViolation(FMLFingerprintViolationEvent event)
+    {
+        // Not running in a dev environment
+        if (event.isDirectory() == false)
+        {
+            logger.warn("*********************************************************************************************");
+            logger.warn("*****                                    WARNING                                        *****");
+            logger.warn("*****                                                                                   *****");
+            logger.warn("*****   The signature of the mod file '{}' does not match the expected fingerprint!     *****", event.getSource().getName());
+            logger.warn("*****   This might mean that the mod file has been tampered with!                       *****");
+            logger.warn("*****   If you did not download the mod {} directly from Curse/CurseForge,       *****", Reference.MOD_NAME);
+            logger.warn("*****   or using one of the well known launchers, and you did not                       *****");
+            logger.warn("*****   modify the mod file at all yourself, then it's possible,                        *****");
+            logger.warn("*****   that it may contain malware or other unwanted things!                           *****");
+            logger.warn("*********************************************************************************************");
+        }
     }
 }
--- src/main/java/fi/dy/masa/worldutils/reference/Reference.java
+++ src/main/java/fi/dy/masa/worldutils/reference/Reference.java
@@ -5,6 +5,8 @@ public class Reference
     public static final String MOD_ID = "worldutils";
     public static final String MOD_NAME = "World Utils";
     public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
+
     public static final String PROXY_CLASS_CLIENT = "fi.dy.masa.worldutils.proxy.ClientProxy";
     public static final String PROXY_CLASS_SERVER = "fi.dy.masa.worldutils.proxy.ServerProxy";
 }
